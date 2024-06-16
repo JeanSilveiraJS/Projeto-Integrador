54c389b5c6a726b68eb26e2461ec27d730661bd9
Jean Silveira <jean.silveira@acad.ufsm.br>
2024-06-16 11:10:31 -0300

    commit inicial

---
+++ src/main/java/br/ufsm/csi/gpsmanager/GpsManagerApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.gpsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufsm/csi/gpsmanager/GpsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.gpsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
