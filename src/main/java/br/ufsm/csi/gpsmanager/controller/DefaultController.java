a863516bf4de927bc528f1b51ec58ba4d68c714c
Jean Silveira <jean.silveira@acad.ufsm.br>
2024-07-20 18:26:09 -0300

    envio de disp para tela inicial

--- src/main/java/br/ufsm/csi/gpsmanager/controller/DefaultController.java
+++ src/main/java/br/ufsm/csi/gpsmanager/controller/DefaultController.java
@@ -1,8 +1,10 @@
 package br.ufsm.csi.gpsmanager.controller;
 
 import br.ufsm.csi.gpsmanager.model.situacao.Situacao;
+import br.ufsm.csi.gpsmanager.service.DispositivoService;
 import br.ufsm.csi.gpsmanager.service.UsuarioService;
 import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,14 +14,12 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/")
+@RequiredArgsConstructor
 public class DefaultController {
     private final HttpSession session;
     private final UsuarioService usuarioService;
+    private final DispositivoService dispositivoService;
 
-    public DefaultController(HttpSession session, UsuarioService usuarioService) {
-        this.session = session;
-        this.usuarioService = usuarioService;
-    }
 
     @GetMapping({"/home", "/"})
     public String home(Model model) {
@@ -32,10 +32,9 @@ public class DefaultController {
         Long idUsuario = Long.valueOf(idUsuarioObj.toString());
         List<Situacao> situacoes = usuarioService.buscarUsuario(idUsuario).getSituacoes();
 
-        //List<Dispositivo> dispositivos = new ArrayList<>();
 
         model.addAttribute("situacoes", situacoes);
-        //model.addAttribute("dispositivos", dispositivos);
+        model.addAttribute("dispositivos", dispositivoService.listarDispositivos());
 
         return "home";
     }
